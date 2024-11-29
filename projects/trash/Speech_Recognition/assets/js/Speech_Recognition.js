c35f29601795fdbc828d2620334fa3cdfe4e507e
Vuila9 <khoa.nguynxun@gmail.com>
2024-11-29 15:42:44 -0500

    added a trash folder to contain benched project(s)

---
+++ projects/trash/Speech_Recognition/assets/js/Speech_Recognition.js
@@ -0,0 +1,57 @@
+let mediaRecorder;
+let audioChunks = [];
+let MEDIA_STREAM = null;
+
+async function initMicrophone() {
+    try {
+        MEDIA_STREAM = await navigator.mediaDevices.getUserMedia({ audio: true });
+        document.getElementById('status').textContent = 'Status: Microphone access granted. Ready to record.';
+    } catch (err) {
+        document.getElementById('status').textContent = 'Status: Error accessing microphone: ' + err;
+        console.error('Error accessing the microphone:', err);
+    }
+}
+
+async function startRecording() {
+    if (!MEDIA_STREAM) {
+        //await initMicrophone(); // Ensure the microphone is initialized
+        document.getElementById('status').textContent = 'Status: Grant permission to use microphone.';
+        return;
+    }
+
+    if (MEDIA_STREAM) {
+        mediaRecorder = new MediaRecorder(MEDIA_STREAM);
+        mediaRecorder.start();
+        document.getElementById('status').textContent = 'Status: Recording...';
+
+        mediaRecorder.ondataavailable = function(event) {
+            audioChunks.push(event.data);
+        };
+
+        // mediaRecorder.onstop = function() {
+        //     const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+        //     const audioUrl = URL.createObjectURL(audioBlob);
+        //     const audio = new Audio(audioUrl);
+        //     audio.play(); // Play the recorded audio after stopping
+        //     audioChunks = []; // Clear for the next recording
+        // };
+    }
+}
+
+function stopRecording() {
+    if (!MEDIA_STREAM) return;
+    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+        mediaRecorder.stop();
+        document.getElementById('status').textContent = 'Status: Stopped recording.';
+    }
+    console.log(audioChunks);
+}
+
+document.getElementById("MAP-button-play").onclick = function() {
+    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    const audioUrl = URL.createObjectURL(audioBlob);
+    const audio = new Audio(audioUrl);
+    audio.play(); // Play the recorded audio after stopping
+    audioChunks = []; // Clear for the next recording
+}
+
---
+++ projects/trash/Speech_Recognition/assets/js/Speech_Recognition_helper.js
@@ -0,0 +1,28 @@
+// Get the image element
+const imageButton = document.getElementById('imageButton');
+
+// Set the images to be used
+const originalImage = './assets/img/green.png';
+const clickedImage = './assets/img/red.png';
+
+// Add event listeners for mouse events
+imageButton.addEventListener('mousedown', () => {
+    imageButton.src = clickedImage;
+    //requestPermission();
+    startRecording();
+
+});
+
+imageButton.addEventListener('mouseup', () => {
+    imageButton.src = originalImage;
+    stopRecording();
+});
+
+imageButton.addEventListener('mouseleave', () => {
+    imageButton.src = originalImage;
+});
+
+window.onload = function() {
+    initMicrophone();
+    //initSpeechRecognition();
+}
