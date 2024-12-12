b6a3f0c2ea98c08fb077a0b4aec4f5da724c6795
Vuila9 <khoa.nguynxun@gmail.com>
2024-12-11 21:41:10 -0500

    added color slider and a temporary eraser

--- projects/MS_Paint/assets/js/MS_paint.js
+++ projects/MS_Paint/assets/js/MS_paint.js
@@ -1,8 +1,14 @@
-const COLOR = 'red';
+let COLOR = 'black';
+let RED = 0;
+let GREEN = 0;
+let BLUE = 0;
+let SIZE = 5;
+
 const mspaint_body = document.getElementById('mspaint-body');
 const locations = new Set();
 let isDrawing = false;
 let isMouseInside = false;
+let isEraserON = false;
 
 document.getElementById('ms-paint').addEventListener('contextmenu', (event) => {
     event.preventDefault(); // Prevent the context menu from showing
@@ -50,6 +56,8 @@ function placePixel(event) {
     pixel.className = 'pixel';
     pixel.style.left = `${x}px`;
     pixel.style.top = `${y}px`;
+    pixel.style.width = `${SIZE}px`;
+    pixel.style.height = `${SIZE}px`;
 
     let currentColor = COLOR;
     pixel.style.backgroundColor = currentColor;
@@ -58,10 +66,50 @@ function placePixel(event) {
     //console.log(location);
 }
 
-function eraseAll() {
-    mspaint_body.innerHTML = '';
+function eraserON() {
+    COLOR = `rgb(242,242,242)`;
+    isEraserON = !isEraserON;
+
+    if (isEraserON) {
+        document.getElementById('eraser').className = 'fa fa-eraser button primary';
+        document.getElementById('eraser').title = 'Eraser is ON';
+        SIZE = 20;
+        document.getElementById('color-slider-red').disabled = true;
+        document.getElementById('color-slider-green').disabled = true;
+        document.getElementById('color-slider-blue').disabled = true;
+
+    }
+    else {
+        document.getElementById('eraser').className = 'fa fa-eraser';
+        document.getElementById('eraser').title = 'Eraser is OFF';
+        SIZE = 5;
+        document.getElementById('color-slider-red').disabled = false;
+        document.getElementById('color-slider-green').disabled = false;
+        document.getElementById('color-slider-blue').disabled = false;
+        COLOR = `rgb(${RED},${GREEN},${BLUE})`;
+    }
+
 }
 
-// window.onload = function() {
-//     eraseAll();
-// }
+function eraseAll() {
+    mspaint_body.innerHTML = '';
+    locations.clear();
+}
+
+function updateColorRED(value) {
+    RED = value;
+    document.getElementById('color-picker').style.backgroundColor = `rgb(${RED},${GREEN},${BLUE})`;
+    COLOR = `rgb(${RED},${GREEN},${BLUE})`;
+}
+
+function updateColorGREEN(value) {
+    GREEN = value;
+    document.getElementById('color-picker').style.backgroundColor = `rgb(${RED},${GREEN},${BLUE})`;
+    COLOR = `rgb(${RED},${GREEN},${BLUE})`;
+}
+
+function updateColorBLUE(value) {
+    BLUE = value;
+    document.getElementById('color-picker').style.backgroundColor = `rgb(${RED},${GREEN},${BLUE})`;
+    COLOR = `rgb(${RED},${GREEN},${BLUE})`;
+}
