54f77cf863910e67516d483fa8b1dd1dc3014083
Vuila9 <khoa.nguynxun@gmail.com>
2024-12-18 17:36:37 -0500

    added game interface (game area, buttons)

---
+++ projects/Snake/assets/js/Snake.js
@@ -0,0 +1,68 @@
+let GAME_LOOP = false;
+let SNAKE_COLOR = 'rgb(255,0,0)';
+let SNAKE_SIZE = 10;
+let GAME_INTERVAL;
+let SNAKE_SPEED = 200;
+let LAST_FRAME_TIME;
+let SNAKE_LENGTH = 1;
+
+const SNAKE_BODY = [];
+let TURN_COORDINATE = [];
+
+const GAME_INTERFACE = document.getElementById('game-interface');
+const MAX_ARENA_WIDTH = GAME_INTERFACE.offsetWidth;
+const MAX_ARENA_HEIGHT = GAME_INTERFACE.offsetHeight;
+
+
+document.getElementById('button-play').addEventListener('mousedown', (event) => {
+    if (event.button == 2) event.preventDefault(); // prevent right-click
+    GAME_LOOP = !GAME_LOOP;
+
+    if (GAME_LOOP) {
+        document.getElementById('button-play-icon').className = 'fa fa-pause';
+        event.target.title = 'Press to pause';
+        LAST_FRAME_TIME = performance.now();
+        gameLoop(LAST_FRAME_TIME);
+    }
+    else {
+        document.getElementById('button-play-icon').className = 'fa fa-play';
+        event.target.title = 'Press to play';
+    }
+});
+
+function gameLoop(currentTime) {
+    if (!GAME_LOOP) return;
+    const elapsedTime = currentTime - LAST_FRAME_TIME;
+
+    if (elapsedTime > 16.67) { // Approx 60 FPS (1000ms/60 = ~16.67ms per frame)
+        const snake = document.getElementById('snake-head');
+        const currentLeft = parseInt(snake.style.left || '0', 10);
+
+        // Calculate movement based on elapsed time
+        const pixelsToMove = (elapsedTime / 1000) * SNAKE_SPEED;
+        if (snake.offsetLeft < 0 - SNAKE_SIZE)
+            snake.style.left = (MAX_ARENA_WIDTH + currentLeft - pixelsToMove) + 'px';
+        else
+            snake.style.left = (currentLeft - pixelsToMove) + 'px';
+
+        // Update the last frame time
+        LAST_FRAME_TIME = currentTime;
+    }
+    
+    // Schedule the next frame
+    requestAnimationFrame(gameLoop);
+}
+
+
+window.onload = function () {
+    const pixel = document.createElement('div');
+    pixel.id = 'snake-head'
+    pixel.className = 'pixel';
+    pixel.style.left = `${Math.floor(MAX_ARENA_WIDTH/2)}px`;
+    pixel.style.top = `${Math.floor(MAX_ARENA_HEIGHT/2)}px`;
+    pixel.style.width = `${SNAKE_SIZE}px`;
+    pixel.style.height = `${SNAKE_SIZE}px`;
+    pixel.style.backgroundColor = SNAKE_COLOR;
+
+    GAME_INTERFACE.appendChild(pixel);
+}
