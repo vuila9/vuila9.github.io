0522bb4c7926120653d07578ba6c4def312314c3
Vuila9 <khoa.nguynxun@gmail.com>
2024-12-22 17:41:14 -0500

    make playfield grid size dynamically adjust based on current browser size, added class template

--- projects/Snake/assets/js/Snake.js
+++ projects/Snake/assets/js/Snake.js
@@ -1,19 +1,25 @@
-function init() {
-    const gridContainer = document.getElementById("game-interface");
+function createGridPlayfield() {
+    const gameinterface = document.getElementById("game-interface");
+    const max_playfield_width = gameinterface.offsetWidth;
+    const max_playfield_height = gameinterface.offsetHeight;
+    const gridsize = 15;
 
-    const rows = 35;
-    const cols = 70;
+    const rows = Math.floor(max_playfield_height / gridsize);
+    const cols = Math.floor(max_playfield_width/ gridsize);
+
+    gameinterface.style.gridTemplateRows = `repeat(${rows}, ${gridsize}px)`
+    gameinterface.style.gridTemplateColumns = `repeat(${cols}, ${gridsize}px)`;
 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
             const box = document.createElement("div");
             box.classList.add("grid-box");
             box.id = `${x},${y}`; // Assign an ID based on coordinates
-            gridContainer.appendChild(box);
+            gameinterface.appendChild(box);
         }
     }
 }
 
 window.onload = function() {
-    init();
+    createGridPlayfield();
 }
---
+++ projects/Snake/assets/js/Snake_DS.js
@@ -0,0 +1,11 @@
+class PlayField {
+
+}
+
+class Snake {
+
+}
+
+class Apple {
+    
+}
