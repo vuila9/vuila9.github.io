2dab107d50118e3076ec3cbcda8f38933f89641f
Vuila9 <khoa.nguynxun@gmail.com>
2025-01-29 05:22:48 -0500

    minor fixes

--- projects/Stream_Simulator/assets/js/Stream_Simulator_class_chat.js
+++ projects/Stream_Simulator/assets/js/Stream_Simulator_class_chat.js
@@ -55,7 +55,7 @@ class ChatDisplay {
 
     addMessage(message) { 
         const USER = message.getUser();
-        if (USER.isbanned()) return; //dead man dont talk
+        if (USER.isBanned()) return; //dead man dont talk
         const messageElement = document.createElement('p');
         messageElement.setAttribute('user', USER.getUsername());
 
