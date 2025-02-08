863eb7cbe5b2acf1a5d61a65b7d32a1b853224d1
Vuila9 <khoa.nguynxun@gmail.com>
2025-02-08 17:29:39 -0500

    dumped code

--- projects/Stream_Simulator/assets/misc/dumped_code.js
+++ projects/Stream_Simulator/assets/misc/dumped_code.js
@@ -18,3 +18,19 @@ function smartSplit(sentence) {
     arr.push(parts);
     return arr;
 }
+
+spamChat(VIEWERS, msg, duration_=null) {
+    if (this.isPause) return;
+    if (Number(this.fakeViewCount) < 50) return;
+    document.getElementById('start-chat-button').disabled = true;
+    const spam_chat = this.#spamVariation(msg);
+    const chat_rate = Math.min(this.chatRate/2, 800);
+    const intervalId = setInterval(() => {
+        this.addMessage(new ChatMessage(VIEWERS[this.#getRand(VIEWERS.length)], spam_chat[this.#getRand(spam_chat.length)]));
+    }, chat_rate);
+    let duration = (duration_ === null) ? Math.max(Math.min(6900 * 500/this.chatRate, 13000), 5000) : duration_;
+    setTimeout(() => {
+        clearInterval(intervalId); 
+        document.getElementById('start-chat-button').disabled = false;
+    }, duration);
+}
